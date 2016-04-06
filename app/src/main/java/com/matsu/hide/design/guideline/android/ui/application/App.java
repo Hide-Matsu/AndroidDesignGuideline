e9312c7ebf4f34521f5af593afc4fd534fdc5ffa
h-matsunaga <h.matsunaga@team.nri-net.com>
2016-04-06 13:04:29 +0900

    ・Applicationクラス作成

---
+++ app/src/main/java/com/matsu/hide/design/guideline/android/ui/application/App.java
@@ -0,0 +1,70 @@
+package com.matsu.hide.design.guideline.android.ui.application;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * Applicationクラス
+ */
+public class App extends Application implements Application.ActivityLifecycleCallbacks {
+
+    //region field
+
+    /**
+     * TAG
+     */
+    public static final String TAG = App.class.getSimpleName();
+
+    //endregion
+
+    //region method
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+    }
+
+    //endregion
+
+}
